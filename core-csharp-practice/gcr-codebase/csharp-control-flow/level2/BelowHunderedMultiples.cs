a2d402a61e6ee6726a56a9dd5db447e2e1a8444f
KshitizAgarwal1359 <122371054+KshitizAgarwal1359@users.noreply.github.com>
2025-12-22 19:13:05 +0530

    [Kshitiz Agarwal] Done with control flow level2 practice questions

--- core-csharp-practice/gcr-codebase/csharp-control-flow/level2/BelowHunderedMultiples.cs
+++ core-csharp-practice/gcr-codebase/csharp-control-flow/level2/BelowHunderedMultiples.cs
@@ -6,7 +6,7 @@ class BelowHunderedMultiples
         Console.Write("Enter a number:");
         int number=Convert.ToInt32(Console.ReadLine());
         Console.WriteLine($"Multiples of {number} below 100 are: ");
-        for(int i = 100; i >= 1; i--)
+        for(int i = 100; i >= 1; i--) //calculating multiples below 100 using for loop
         {
             if (i % number == 0)
             {
