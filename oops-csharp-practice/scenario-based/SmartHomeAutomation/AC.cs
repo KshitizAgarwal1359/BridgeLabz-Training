b0d0ceb054ae690b5112739a3d2de8ade612753a
KshitizAgarwal1359 <122371054+KshitizAgarwal1359@users.noreply.github.com>
2026-01-08 16:08:57 +0530

    [Kshitiz Agarwal] added the smart home appliance scenario question

---
+++ oops-csharp-practice/scenario-based/SmartHomeAutomation/AC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.SmartHomeAutomation
+{
+    class AC : Appliance
+    {
+        public AC(string name) : base(name) { }
+        public override void TurnOn()
+        {
+            Console.WriteLine($"{Name} AC in turned on on 24C.");
+        }
+        public override void TurnOff()
+        {
+            Console.WriteLine($"{Name} AC is turned off.");
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/SmartHomeAutomation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.SmartHomeAutomation
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Appliance[] appliances =
+            {
+                new Light("Bathroom Light"),
+                new Fan("Bedroom Fan"),
+                new AC("Living Room")
+            };
+            foreach (Appliance appliance in appliances)
+            {
+                appliance.TurnOn();
+                appliance.TurnOff();
+                Console.WriteLine();
+            }
+        }
+    }
+}
