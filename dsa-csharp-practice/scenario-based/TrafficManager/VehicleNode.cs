7f0b2573371cfc04966eff8d5f687e904abe80b9
KshitizAgarwal1359 <122371054+KshitizAgarwal1359@users.noreply.github.com>
2026-01-16 16:09:27 +0530

    [Kshitiz Agarwal] added the traffic manager problem

---
+++ dsa-csharp-practice/scenario-based/TrafficManager/VehicleNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzDSA.Scenario_Based.TrafficManager
+{
+    class VehicleNode //represents single vehicle
+    {
+        public string VehicleNumber;
+        public VehicleNode Next;
+        public VehicleNode(string vehicleNumber)
+        {
+            VehicleNumber = vehicleNumber;
+            Next = null;
+        }
+    }
+}
