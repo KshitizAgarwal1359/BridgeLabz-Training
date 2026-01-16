c57fa21745dd4483e220629a6926e637441d0670
KshitizAgarwal1359 <122371054+KshitizAgarwal1359@users.noreply.github.com>
2026-01-16 12:16:55 +0530

    [Kshitiz Agarwal] added the browser buddy problem

---
+++ dsa-csharp-practice/scenario-based/BrowserBuddy/Tab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzDSA.Scenario_Based.BrowserBuddy
+{
+    class Tab
+    {
+        public DoublyLinkedHistory history;
+        public Tab next; //used for stack linking
+        public Tab()
+        {
+            history = new DoublyLinkedHistory();  //creating a doubly linked list
+            next = null;
+        }
+    }
+}
