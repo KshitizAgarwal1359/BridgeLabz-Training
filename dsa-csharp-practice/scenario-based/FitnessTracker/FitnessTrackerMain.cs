2ddf8ea9345a19bc185f19bff60d16360c83162f
KshitizAgarwal1359 <122371054+KshitizAgarwal1359@users.noreply.github.com>
2026-01-17 16:01:12 +0530

    [Kshitiz Agarwal] added the scenario question for sorting

---
+++ dsa-csharp-practice/scenario-based/FitnessTracker/FitnessTrackerMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzDSA.Scenario_Based.FitnessTracker
+{
+    class FitnessTrackerMain
+    {
+        public static void Main(String[] args)
+        {
+            User[] users =
+            {
+                new User{userId = 343,userName="Ravi",steps=2345},
+                new User{userId=456,userName="Aarush",steps=6790},
+                new User{userId=455,userName="Dhruv",steps=5600},
+                new User{userId=245,userName="Kshitiz",steps=8900},
+                new User{userId=1222,userName="Sameer",steps=1500},
+                new User{userId=2367,userName="Shashi",steps=8872},
+                new User{userId=567,userName="Anshika",steps=908},
+            };
+            Console.WriteLine("Before Sorting: \n");
+            PrintUsers(users);
+            BubbleSort.Sort(users);
+            Console.WriteLine("After sorting: \n");
+            PrintUsers(users);
+
+        }
+        static void PrintUsers(User[] users)
+        {
+            foreach(var user in users)
+            {
+                Console.WriteLine(user);
+            }
+            Console.WriteLine();
+        }
+    }
+}
