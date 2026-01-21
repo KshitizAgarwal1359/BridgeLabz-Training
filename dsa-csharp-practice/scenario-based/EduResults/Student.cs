16f840ea8d40c099c6b0975887043356b56c552f
KshitizAgarwal1359 <122371054+KshitizAgarwal1359@users.noreply.github.com>
2026-01-21 15:26:48 +0530

    [Kshitiz Agarwal] added the edu result problem

---
+++ dsa-csharp-practice/scenario-based/EduResults/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BridgeLabzDSA.Scenario_Based.EduResults
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
+        public Student(int rollNo, string name, int marks)
+        {
+            RollNo = rollNo;
+            Name = name;
+            Marks = marks;
+        }
+    }
+}
